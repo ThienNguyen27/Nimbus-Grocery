e74b62c92a7607e7ef136c7a4de4bf8b848880ab
thien quach chi <promaster30525@gmail.com>
2025-06-29 12:38:06 +0700

    check5

---
+++ convex/auth.config.ts
@@ -0,0 +1,10 @@
+const config = {
+    providers: [
+      {
+        domain: "https://civil-pony-36.clerk.accounts.dev",
+        applicationID: "convex",
+      },
+    ]
+  };
+
+export default config;
