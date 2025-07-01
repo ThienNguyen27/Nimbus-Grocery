04bafcaae91aa2477e97313135e57564cec9704d
thiennguyen27 <nguyenleminhthien27092005@gmail.com>
2025-06-30 21:37:02 -0500

    update sign up

--- next.config.ts
+++ next.config.ts
@@ -11,14 +11,14 @@ module.exports = {
   async redirects() {
     return [
       {
-        source:      '/signup',                            // catch /signup on port 3000
+        source: '/signup',                            // catch /signup on port 3000
         destination: 'http://127.0.0.1:8000/signup',    // send to FastAPI
-        permanent:   false,                                // 307 temporary redirect
+        permanent: false,                                // 307 temporary redirect
       },
       {
-        source:      '/signup/',                           // also catch trailing slash
+        source: '/signup/',                           // also catch trailing slash
         destination: 'http://127.0.0.1:8000/signup/',
-        permanent:   false,
+        permanent: false,
       },
     ];
   },
