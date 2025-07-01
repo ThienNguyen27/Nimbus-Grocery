d96d7bbefff9b3901e495ea407710fe609f7b551
thien quach chi <promaster30525@gmail.com>
2025-07-01 07:10:32 +0700

    sign up page api

--- next.config.ts
+++ next.config.ts
@@ -12,12 +12,12 @@ module.exports = {
     return [
       {
         source:      '/signup',                            // catch /signup on port 3000
-        destination: 'http://192.168.68.62:8000/signup',    // send to FastAPI
+        destination: 'http://127.0.0.1:8000/signup',    // send to FastAPI
         permanent:   false,                                // 307 temporary redirect
       },
       {
         source:      '/signup/',                           // also catch trailing slash
-        destination: 'http://192.168.68.62:8000/signup/',
+        destination: 'http://127.0.0.1:8000/signup/',
         permanent:   false,
       },
     ];
